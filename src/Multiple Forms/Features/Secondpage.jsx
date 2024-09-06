6617b21fef0e8d20f7af728c57ae7973c6c80f70
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-06 14:15:22 +0100

    updated

--- src/Multiple Forms/Features/Secondpage.jsx
+++ src/Multiple Forms/Features/Secondpage.jsx
@@ -3,7 +3,6 @@ import { useDispatch } from "react-redux";
 import { updateFormData } from "./Slice/FormSlice";
 import { useNavigate } from "react-router-dom";
 
-
 const SecondPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -18,31 +17,41 @@ const SecondPage = () => {
     navigate("/third");
   };
 
+  const Previous = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
   return (
-    <div className="p-4 border rounded-md">
-      <label htmlFor="phone">Phone Number</label>
-      <input
-        type="text"
-        name="phoneNumber"
-        onInput={handleInput}
-        required
-        className="border rounded px-2 py-1 w-full"
-      />
-      <br />
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        name="email"
-        onInput={handleInput}
-        required
-        className="border rounded px-2 py-1 w-full"
-      />
-      <br />
-      <button
-        onClick={Next}
-        className="mt-4 bg-[#ff000064] text-white text-xl p-2 w-full rounded hover:bg-[#0000ff45]"
-      >
-        Next
+    <div>
+      <div className="p-4 border rounded-md">
+        <label htmlFor="phone">Phone Number</label>
+        <input
+          type="text"
+          name="phoneNumber"
+          onInput={handleInput}
+          required
+          className="border rounded px-2 py-1 w-full"
+        />
+        <br />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          onInput={handleInput}
+          required
+          className="border rounded px-2 py-1 w-full"
+        />
+        <br />
+        <button
+          onClick={Next}
+          className="mt-4 bg-[#ff000064] text-white text-xl p-2 w-full rounded hover:bg-[#0000ff45]"
+        >
+          Next
+        </button>
+      </div>
+      <button onClick={Previous} className="p-10 text-red-600 text-[10px]">
+        Go Back To First Page
       </button>
     </div>
   );
--- src/Multiple Forms/Features/ThirdPage.jsx
+++ src/Multiple Forms/Features/ThirdPage.jsx
@@ -3,7 +3,6 @@ import { useDispatch } from "react-redux";
 import { updateFormData } from "./Slice/FormSlice";
 import { useNavigate } from "react-router-dom";
 
-
 const ThirdPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -18,31 +17,41 @@ const ThirdPage = () => {
     navigate("/submit");
   };
 
+  const Previous = (e) => {
+    e.preventDefault();
+    navigate("/second");
+  };
+
   return (
-    <div className="p-4 border rounded-md">
-      <label htmlFor="address">Address</label>
-      <input
-        type="text"
-        name="address"
-        onInput={handleInput}
-        required
-        className="border rounded px-2 py-1 w-full"
-      />
-      <br />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        onInput={handleInput}
-        required
-        className="border rounded px-2 py-1 w-full"
-      />
-      <br />
-      <button
-        onClick={Submit}
-        className="mt-4 bg-[#ff000064] text-white text-xl p-2 w-full rounded hover:bg-[#0000ff45]"
-      >
-        Submit
+    <div>
+      <div className="p-4 border rounded-md">
+        <label htmlFor="address">Address</label>
+        <input
+          type="text"
+          name="address"
+          onInput={handleInput}
+          required
+          className="border rounded px-2 py-1 w-full"
+        />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          onInput={handleInput}
+          required
+          className="border rounded px-2 py-1 w-full"
+        />
+        <br />
+        <button
+          onClick={Submit}
+          className="mt-4 bg-[#ff000064] text-white text-xl p-2 w-full rounded hover:bg-[#0000ff45]"
+        >
+          Submit
+        </button>
+      </div>
+      <button onClick={Previous} className="p-10 text-red-600 text-[10px]">
+        Go Back To Second Page
       </button>
     </div>
   );
